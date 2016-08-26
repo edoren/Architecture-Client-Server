99aa94c8d22556e9b875ad0d3ac98fadaddd3ef7
Manuel Sabogal <mfer32@gmail.com>
2016-08-26 10:44:36 -0500

    Fixed bug where operator>> is extracting the next value.

--- src/Util/ZMQPPAdaptors.hpp
+++ src/Util/ZMQPPAdaptors.hpp
@@ -10,9 +10,10 @@ zmqpp::message& operator<<(zmqpp::message& msg, const Buffer& buffer) {
 }
 
 zmqpp::message& operator>>(zmqpp::message& msg, Buffer& buffer) {
-    size_t part = msg.next();
+    size_t part = msg.read_cursor();
     const char* data = static_cast<const char*>(msg.raw_data(part));
     size_t len = msg.size(part);
+    msg.next();
     buffer.write(data, len);
     return msg;
 }
